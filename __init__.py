56aab4f713d6ef11936a6f46475cf34d418f149a
Moody Liu <mooodyhunter@outlook.com>
2022-07-10 16:37:46 +0100

    code cleanup

--- __init__.py
+++ __init__.py
@@ -33,6 +33,28 @@ class TgUserBot(Client):
 UserBot = TgUserBot(Config.BOT_SESSION_NAME, Config.APP_ID, Config.API_HASH)
 
 
+async def process_message(message, **kwargs):
+    futures = [r.create_task(message) for r in REGISTRATIONS if r.matches(message, **kwargs)]
+    if futures:
+        await asyncio.wait(futures)
+
+
+@UserBot.on_message(filters=pyrogram.filters.all)
+async def new_message_handler_impl(_, message: pyrogram.types.Message):
+    await process_message(message)
+
+
+@UserBot.on_deleted_messages(filters=pyrogram.filters.all)
+async def deleted_message_handler_impl(_, message: pyrogram.types.Message):
+    for m in message:
+        await process_message(m, isDeleted=True)
+
+
+@UserBot.on_edited_message(filters=pyrogram.filters.all)
+async def edited_message_handler_impl(_, message: pyrogram.types.list.List):
+    await process_message(message, isEdited=True)
+
+
 class Registration:
     def __init__(self,
                  module: str,
--- __main__.py
+++ __main__.py
@@ -1,30 +1,4 @@
-import asyncio
-
-import pyrogram
-
-from userbot import REGISTRATIONS, StartBot, UserBot, load_plugin
-
-
-async def process_message(message, **kwargs):
-    futures = [r.create_task(message) for r in REGISTRATIONS if r.matches(message, **kwargs)]
-    if futures:
-        await asyncio.wait(futures)
-
-
-@UserBot.on_message(filters=pyrogram.filters.all)
-async def new_message_handler_impl(_, message: pyrogram.types.Message):
-    await process_message(message)
-
-
-@UserBot.on_deleted_messages(filters=pyrogram.filters.all)
-async def deleted_message_handler_impl(_, message: pyrogram.types.Message):
-    for m in message:
-        await process_message(m, isDeleted=True)
-
-
-@UserBot.on_edited_message(filters=pyrogram.filters.all)
-async def edited_message_handler_impl(_, message: pyrogram.types.list.List):
-    await process_message(message, isEdited=True)
+from userbot import StartBot
 
 if __name__ == "__main__":
     StartBot()
